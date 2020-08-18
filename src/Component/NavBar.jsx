bf667a33e11ccda371076c1f29a648ec8370965f
Ibrahim Kurhan <ikurhan@outlook.com>
2020-08-18 12:48:02 +0100

    navbar styled

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Input, Menu } from "semantic-ui-react";
 
 function NavBar() {
   return (
     <div>
-      <p></p>
+      <Menu inverted color="blue" size="huge">
+        <Menu.Item name="home" />
+        <Menu.Item name="messages" />
+        <Menu.Item name="friends" />
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Input icon="search" placeholder="Search..." />
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
     </div>
   );
 }
--- src/Container/App.js
+++ src/Container/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import NavBar from "../Component/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <div>Hello world</div>
+      <NavBar />
     </div>
   );
 }
