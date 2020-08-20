87d4fac0528cb2a92c42dd7d40a3eb34afec18fb
Ibrahim Kurhan <ikurhan@outlook.com>
2020-08-21 00:49:13 +0100

    page not found added

--- src/Component/NotFound.jsx
+++ src/Component/NotFound.jsx
@@ -4,7 +4,12 @@ export default class NotFound extends Component {
   render() {
     return (
       <div>
-        <h3>page not found</h3>
+        <h3>404 page not found</h3>
+        <p>
+          the requested url <span>{this.props.location.pathname}</span>
+          <br />
+          was not found
+        </p>
       </div>
     );
   }
