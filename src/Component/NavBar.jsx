a5be3f0c8c5543616666d5ee5c263a20fc714656
Ibrahim Kurhan <ikurhan@outlook.com>
2020-08-21 09:20:55 +0100

    bug fixed i navbar

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -1,31 +1,27 @@
 import React from "react";
 import { Input, Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
-import { ProductConsumer } from "../Component/Context";
 
-const NavBar = ({ value }) => {
-  const { basket } = value;
+const NavBar = () => {
   return (
     <div>
-      <ProductConsumer>
-        <Menu inverted style={{ borderRadius: "0" }} size="huge">
-          <Menu.Item>
-            E-COMMERCE <br></br>APP
-          </Menu.Item>
-          <Menu.Item>
-            <Link to="/">Products</Link>
-          </Menu.Item>
-          <Menu inverted floated="right" size="huge">
-            <Menu.Item>
-              <Link to="/basket">Your Basket:{basket.length}</Link>
-            </Menu.Item>
-          </Menu>
-        </Menu>
-
+      <Menu inverted style={{ borderRadius: "0" }} size="huge">
         <Menu.Item>
-          <Input icon="search" placeholder="Search..." />
+          E-COMMERCE <br></br>APP
         </Menu.Item>
-      </ProductConsumer>
+        <Menu.Item>
+          <Link to="/">Products</Link>
+        </Menu.Item>
+        <Menu inverted floated="right" size="huge">
+          <Menu.Item>
+            <Link to="/basket">Your Basket:8</Link>
+          </Menu.Item>
+        </Menu>
+      </Menu>
+
+      <Menu.Item>
+        <Input icon="search" placeholder="Search..." />
+      </Menu.Item>
     </div>
   );
 };
