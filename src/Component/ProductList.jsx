211e04baad8c2be59c0497e867365d99e1049a74
Ibrahim Kurhan <ikurhan@outlook.com>
2020-09-07 20:31:32 +0100

    pagination has been removed

--- src/Component/ProductList.jsx
+++ src/Component/ProductList.jsx
@@ -16,7 +16,6 @@ const ProductList = () => {
                 });
               }}
             </ProductConsumer>
-            {/* <Pagination /> */}
           </div>
         </div>
       </div>
