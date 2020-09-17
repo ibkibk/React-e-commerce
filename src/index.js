be2a7568f51b8188a9feb1ef3fb8df28b0c6ba73
Ibrahim Kurhan <ikurhan@outlook.com>
2020-09-17 13:01:17 +0100

    commented line has been deleted

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-// import { BrowserRouter as Router } from "react-router-dom";
-
 import "semantic-ui-css/semantic.min.css";
 import App from "./Container/App";
 import * as serviceWorker from "./serviceWorker";
