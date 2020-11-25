55aa10856b055cf32d579a920e3f79f40d1be499
Ibrahim Kurhan <ikurhan@outlook.com>
2020-11-25 17:36:34 +0300

    new product added

--- src/Static/Data.js
+++ src/Static/Data.js
@@ -191,6 +191,18 @@ export const storeProducts = [
     count: 0,
     total: 0,
   },
+  {
+    id: 17,
+    title: "Google Pixel - Black",
+    img: "img/product-16.jpeg",
+    price: 800,
+    company: "GOOGLE",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inBasket: false,
+    count: 0,
+    total: 0,
+  },
 ];
 
 export const detailProduct = {
